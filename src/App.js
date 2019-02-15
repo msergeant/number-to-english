a0095d35ba78963cb14089fc2152e4c072e82d52
Mike Sergeant <sergeant.mj@gmail.com>
2019-02-15 10:21:27 -0500

    Start working on the UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import StatFinder from './english_generator/StatFinder'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startInteger: 1,
+      endInteger: 123,
+      targetCharacter: "e",
+      stats: null
+    };
+
+    this._handleInputChange = this._handleInputChange.bind(this);
+    this._handleCalculateClick = this._handleCalculateClick.bind(this);
+  }
+
+  _handleInputChange(ev) {
+    const nextState = {...this.state};
+    nextState[ev.target.name] = ev.target.value;
+    this.setState(nextState);
+  }
+
+  _handleCalculateClick(e) {
+    e.preventDefault();
+    const nextState = {...this.state};
+    nextState.stats = StatFinder.findAllStats(
+      nextState.startInteger,
+      nextState.endInteger,
+      nextState.targetCharacter
+    );
+    this.setState(nextState);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input
+          type='number'
+          name='startInteger'
+          id='startInteger'
+          value={this.state.startInteger}
+          onChange={this._handleInputChange} />
+        <input
+          type='number'
+          name='endInteger'
+          id='endInteger'
+          value={this.state.endInteger}
+          onChange={this._handleInputChange} />
+        <input
+          type='submit'
+          id="calculateButton"
+          className="button"
+          value="Calculate"
+          onClick={this._handleCalculateClick} />
+        <pre>
+          { JSON.stringify(this.state) }
+        </pre>
       </div>
     );
   }
